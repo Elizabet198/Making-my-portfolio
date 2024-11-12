67c282f03ef62bc2024fd72688d7fbb0863e4158
Elizabet R. Meyer <MeyerElizabet@it.net>
2024-11-12 13:36:20 -0500

    2.1

--- Certifications Java Script.js
+++ Certifications Java Script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+// JavaScript Document
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+
+hamburgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("active")
+});
